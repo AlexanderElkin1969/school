ee4ab19db20088dc0fae439f21c693c5fd142aa7
AlexanderElkin1969 <eaa@mfcmmr.ru>
2023-07-14 01:33:39 +0300

    FacultyControllerTest done

--- src/test/java/ru/hogwarts/school/controller/FacultyControllerTest.java
+++ src/test/java/ru/hogwarts/school/controller/FacultyControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.model.Student;
 import ru.hogwarts.school.repository.FacultyRepository;
 import ru.hogwarts.school.repository.StudentRepository;
 
@@ -75,8 +76,8 @@ public class FacultyControllerTest {
 
     @Test
     public void getFacultyTest() throws Exception{
-        when(facultyRepository.findById(eq(4L))).thenReturn(Optional.of(faculty));
-        mockMvc.perform(MockMvcRequestBuilders.get("/faculty/4")
+        when(facultyRepository.findById(eq(id))).thenReturn(Optional.of(faculty));
+        mockMvc.perform(MockMvcRequestBuilders.get("/faculty/"+id)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(id))
@@ -86,10 +87,10 @@ public class FacultyControllerTest {
         Mockito.reset(facultyRepository);
 
         when(facultyRepository.findById(eq(5L))).thenReturn(Optional.empty());
-        mockMvc.perform(MockMvcRequestBuilders.get("/faculty/5")
+        mockMvc.perform(MockMvcRequestBuilders.get("/faculty/"+5)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isNotFound());
-        verify(facultyRepository, only()).findById(any());
+        verify(facultyRepository, only()).findById(eq(5L));
     }
 
     @Test
@@ -115,8 +116,8 @@ public class FacultyControllerTest {
 
     @Test
     public void deleteFacultyTest() throws Exception{
-        when(facultyRepository.findById(eq(4L))).thenReturn(Optional.of(faculty));
-        mockMvc.perform(MockMvcRequestBuilders.delete("/faculty/4")
+        when(facultyRepository.findById(eq(id))).thenReturn(Optional.of(faculty));
+        mockMvc.perform(MockMvcRequestBuilders.delete("/faculty/"+id)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(id))
@@ -125,10 +126,10 @@ public class FacultyControllerTest {
         Mockito.reset(facultyRepository);
 
         when(facultyRepository.findById(eq(5L))).thenReturn(Optional.empty());
-        mockMvc.perform(MockMvcRequestBuilders.delete("/faculty/5")
+        mockMvc.perform(MockMvcRequestBuilders.delete("/faculty/"+5)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isNotFound());
-        verify(facultyRepository, only()).findById(any());
+        verify(facultyRepository, only()).findById(eq(5L));
     }
 
     @Test
@@ -163,8 +164,8 @@ public class FacultyControllerTest {
                 new Faculty(1L, "TestName", "Color"),
                 new Faculty(3L, "Name", "Color")
         ));
-        when(facultyRepository.findAllByColor(any())).thenReturn(faculties);
-        mockMvc.perform(MockMvcRequestBuilders.get("/faculty/color/Color")
+        when(facultyRepository.findAllByColor(eq("color"))).thenReturn(faculties);
+        mockMvc.perform(MockMvcRequestBuilders.get("/faculty/color/color")
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$[0].id").value(1L))
@@ -177,10 +178,80 @@ public class FacultyControllerTest {
         Mockito.reset(facultyRepository);
 
         when(facultyRepository.findAllByColor(any())).thenReturn(new ArrayList<>());
-        mockMvc.perform(MockMvcRequestBuilders.get("/faculty/color/Color")
+        mockMvc.perform(MockMvcRequestBuilders.get("/faculty/color/NewColor")
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
         verify(facultyRepository, only()).findAllByColor(any());
+    }
+
+    @Test
+    public void getAllByColorOrNameIgnoreCaseTest() throws Exception{
+        List<Faculty> facultiesOneColor = new ArrayList<>(List.of(
+                new Faculty(1L, "TestName", "Color"),
+                new Faculty(3L, "Name", "Color")
+        ));
+        when(facultyRepository.findAllByColorIgnoreCaseOrNameIgnoreCase(eq("color"), any()))
+                .thenReturn(facultiesOneColor);
+        mockMvc.perform(MockMvcRequestBuilders.get("/faculty/color/color/orName/NewName")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value(1L))
+                .andExpect(jsonPath("$[0].name").value("TestName"))
+                .andExpect(jsonPath("$[0].color").value("Color"))
+                .andExpect(jsonPath("$[1].id").value(3L))
+                .andExpect(jsonPath("$[1].name").value("Name"))
+                .andExpect(jsonPath("$[1].color").value("Color"));
+        verify(facultyRepository, only()).findAllByColorIgnoreCaseOrNameIgnoreCase(eq("color"), any());
+        Mockito.reset(facultyRepository);
+
+        List<Faculty> facultiesOneName = new ArrayList<>(List.of(
+                new Faculty(2L, "Name", "TestColor"),
+                new Faculty(3L, "Name", "Color")
+        ));
+        when(facultyRepository.findAllByColorIgnoreCaseOrNameIgnoreCase(any(), eq("name")))
+                .thenReturn(facultiesOneName);
+        mockMvc.perform(MockMvcRequestBuilders.get("/faculty/color/NewColor/orName/name")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value(2L))
+                .andExpect(jsonPath("$[0].name").value("Name"))
+                .andExpect(jsonPath("$[0].color").value("TestColor"))
+                .andExpect(jsonPath("$[1].id").value(3L))
+                .andExpect(jsonPath("$[1].name").value("Name"))
+                .andExpect(jsonPath("$[1].color").value("Color"));
+        verify(facultyRepository, only()).findAllByColorIgnoreCaseOrNameIgnoreCase(any(), eq("name"));
+        Mockito.reset(facultyRepository);
+    }
+
+    @Test
+    public void getStudentByFacultyTest() throws Exception{
+        List<Student> students = new ArrayList<>(List.of(
+                new Student(1L, "Student_1", 16, faculty,""),
+                new Student(2L, "Student_2", 17, faculty,"")
+        ));
+        when(facultyRepository.findById(eq(id))).thenReturn(Optional.of(faculty));
+        when(studentRepository.findAllByFaculty(eq(faculty))).thenReturn(students);
+        mockMvc.perform(MockMvcRequestBuilders.get("/faculty/"+id+"/students")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value(1L))
+                .andExpect(jsonPath("$[0].name").value("Student_1"))
+                .andExpect(jsonPath("$[0].age").value(16))
+                .andExpect(jsonPath("$[1].id").value(2L))
+                .andExpect(jsonPath("$[1].name").value("Student_2"))
+                .andExpect(jsonPath("$[1].age").value(17));
+        verify(facultyRepository, only()).findById(eq(id));
+        Mockito.reset(facultyRepository);
+        verify(studentRepository, only()).findAllByFaculty(eq(faculty));
+        Mockito.reset(studentRepository);
+
+        when(facultyRepository.findById(eq(5L))).thenReturn(Optional.empty());
+        when(studentRepository.findAllByFaculty(any())).thenReturn(students);
+        mockMvc.perform(MockMvcRequestBuilders.get("/faculty/"+5+"/students")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+        verify(facultyRepository, only()).findById(eq(5L));
+        verify(studentRepository, times(0)).findAllByFaculty(any());
     }
 
 }
